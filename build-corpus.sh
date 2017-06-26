a69fdb113e04fe112520bb10c0c12d6a813c87a7
Adam Wulf <adam.wulf@gmail.com>
2017-06-26 21:34:49 +0000

    Can accept a file of xml as input, and write output files effeciently. plain text is also saved per file.

--- build-corpus.sh
+++ build-corpus.sh
@@ -1,94 +1,106 @@
 
 n=0;
 size_so_far=0;
+size_per_file=1073741824;
+size_per_write=20000;
+write_buffer="";
 
 while read name
 do
-   if (("$size_so_far" > 1073741824)); then
+   if (("$size_so_far" > "$size_per_file")); then
      size_so_far=0;
      n=$(($n + 1));
    fi
 
    if [[ $name != *"/Wikipedia"* && $name != *"Template%3A"* ]]; then
-        # read in the content
-     	plain_content=$(cat header $name footer);
 
-	echo $name;
-	echo $plain_content;
-	echo "-------------------------------"
+	plain_content="";
+        plainfilename="${name%.*}"
 
-	# remove template links
-        plain_content=$(sed -r -e 's|<a [^>]*>Template:[^>]*</a>||g' <<< $plain_content);
+	if [ -f $plainfilename ]; then
+		plain_content=$(cat $plainfilename);
+	else
+		# read in the content
+		plain_content=$(cat header $name footer);
 
-	# remove <ref> tags
-	plain_content=$(sed -r -e 's|&lt;ref[^&]*&gt;[^&]*&lt;/ref&gt;||g' <<< $plain_content);
+		# remove template links
+		plain_content=$(sed -r -e 's|<a [^>]*>Template:[^>]*</a>||g' <<< $plain_content);
 
+		# remove <ref> tags
+		plain_content=$(sed -r -e 's|&lt;ref[^&]*&gt;[^&]*&lt;/ref&gt;||g' <<< $plain_content);
 
-        echo $plain_content;
-        echo "-------------------------------"
-
-
-	# get only the content of <p> tags
-        plain_content=$(xmllint --xpath "//p//child::text()" --recover --nowarning - 2> /dev/null <<< $plain_content)
+		# get only the content of <p> tags
+		plain_content=$(xmllint --xpath "//p//child::text()" --recover --nowarning - 2> /dev/null <<< $plain_content)
 
 
-        plain_content=$(recode html..utf8 <<< $plain_content)
+		plain_content=$(recode html..utf8 <<< $plain_content)
 
-	if [[ $plain_content != *"&"* ]]; then
-		# convert html entities, if any
-#               plain_content=$(echo $plain_content | perl -MHTML::Entities -pe 'decode_entities($_);' 2> /dev/null)
-        	plain_content=$(echo $plain_content | php -r 'echo html_entity_decode(file_get_contents("php://stdin"), ENT_QUOTES|ENT_HTML401);' 2> /dev/null)
+		if [[ $plain_content != *"&"* ]]; then
+			# convert html entities, if any
+	#               plain_content=$(echo $plain_content | perl -MHTML::Entities -pe 'decode_entities($_);' 2> /dev/null)
+			plain_content=$(echo $plain_content | php -r 'echo html_entity_decode(file_get_contents("php://stdin"), ENT_QUOTES|ENT_HTML401);' 2> /dev/null)
+		fi
+
+		# remove <ref /> tags
+		plain_content=$(sed -r -e 's|<ref([^>]*)/>||g' <<< $plain_content);
+		plain_content=$(sed -r -e 's|<ref[^>]*>| |g' <<< $plain_content);
+		plain_content=$(sed -r -e 's|</ref>| |g' <<< $plain_content);
+
+		# remove " ()" which is leftover from the removed template links above
+		# and trim leading/trailing whitespace
+		plain_content=$(sed -r -e 's|\s\(\)||g' <<< $plain_content);
+
+		# remove lines starting with |
+		plain_content=$(sed '/^|/ d' <<< $plain_content);
+
+		### Trim leading whitespaces ###
+		plain_content="${plain_content##*( )}"
+ 
+		### trim trailing whitespaces  ##
+		plain_content="${plain_content%%*( )}"
+
+        	echo $plain_content >> $plainfilename;
 	fi
 
-	# remove <ref /> tags
-        plain_content=$(sed -r -e 's|<ref([^>]*)/>||g' <<< $plain_content);
-        plain_content=$(sed -r -e 's|<ref[^>]*>| |g' <<< $plain_content);
-        plain_content=$(sed -r -e 's|</ref>| |g' <<< $plain_content);
+#	echo $plain_content;
+#	echo "======================================="
 
-        # remove " ()" which is leftover from the removed template links above
-	# and trim leading/trailing whitespace
-        plain_content=$(sed -r -e 's|\s\(\)||g' <<< $plain_content);
-
-	# remove lines starting with |
-	plain_content=$(sed '/^|/ d' <<< $plain_content);
-
-	### Trim leading whitespaces ###
-	plain_content="${plain_content##*( )}"
- 
-	### trim trailing whitespaces  ##
-	plain_content="${plain_content%%*( )}"
-
-	echo $plain_content;
-	echo "======================================="
-
-
-
-
-	# find out the final byte size of our content
         size_to_append=${#plain_content};
         size_to_append=$(echo -n $plain_content | wc -c);
 
-        # size_to_append=$(du -sb $name | awk '{ print $1 }')
-        size_so_far=$(($size_so_far + $size_to_append));
+	if (("$size_to_append" > 0 )); then
+		write_buffer=$write_buffer$plain_content"\n"
 
-        # $name is the file that we'll be appending to the corpus file
-	target_file=../corpus/out-$n.txt
-        target_size=0;
+		# find out the final byte size of our content
+		size_to_append=${#write_buffer};
+		size_to_append=$(echo -n $write_buffer | wc -c);
 
-        if [ -f $target_file ]; then
-            target_size=$(du -sb $target_file | awk '{ print $1 }')
-        fi
+		if (("$size_to_append" > "$size_per_write")); then
+			# size_to_append=$(du -sb $name | awk '{ print $1 }')
+			size_so_far=$(($size_so_far + $size_to_append));
 
-        if (("$size_so_far" > "$target_size")); then
-            echo $plain_content >> $target_file;
-        fi
+			# $name is the file that we'll be appending to the corpus file
+			target_file=../corpus/out-$n.txt
+			target_size=0;
+
+			if [ -f $target_file ]; then
+				target_size=$(du -sb $target_file | awk '{ print $1 }')
+			fi
+
+			if (("$size_so_far" > "$target_size")); then
+				echo $write_buffer >> $target_file;
+				size_to_append=0;
+				write_buffer="";
+			fi
+		fi
+	fi
    fi
 
 
 # break;
 
 
-done < en.xml.100.files
+done < $1
 
 
 
